42b4c35fbf6f2d9b3c9e40e7e356c94737d0670a
hemanth84-del <99811399+hemanth84-del@users.noreply.github.com>
2022-06-01 18:38:34 +0530

    Add files via upload

---
+++ oop.py
@@ -0,0 +1,6 @@
+class Sample:
+    num1=10
+    def print_var():
+        print('num1 is',Sample.num1)
+print(Sample.num1)
+Sample.print_var()
---
+++ oop1.py
@@ -0,0 +1,6 @@
+from math import pi
+class Area:
+    r=int(input('enter the radius'))
+    def circle():
+        print(f'Area of a circle{Area.r}->is {pi*Area.r}')
+Area.circle()
